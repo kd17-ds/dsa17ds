4995aac07ee418a76d536ac1fd9ff6955fb4e8d6
Divyansh Sharma <shan17div@gmail.com>
2026-01-15 21:14:31 +0530

    Day-6

--- JavaBasics/Patterns.java
+++ JavaBasics/Patterns.java
@@ -101,16 +101,55 @@ public class Patterns {
       // System.out.println();
       // }
 
-      char a = 'E';
-      for (int i = 1; i <= 5; i++) {
-         char b = a;
-         for (int j = 1; j <= i; j++) {
+      // char a = 'E';
+      // for (int i = 1; i <= 5; i++) {
+      // char b = a;
+      // for (int j = 1; j <= i; j++) {
 
-            System.out.print(b);
-            b++;
+      // System.out.print(b);
+      // b++;
+      // }
+      // System.out.println();
+      // a--;
+      // }
+
+      for (int i = 0; i < 5; i++) {
+         for (int j = 5; j > i; j--) {
+            System.out.print("*");
+         }
+         for (int j = 1; j <= 2 * i; j++) {
+            System.out.print(" ");
+         }
+         for (int j = 5; j > i; j--) {
+            System.out.print("*");
          }
          System.out.println();
-         a--;
+      }
+
+      for (int i = 5; i > 0; i--) {
+         for (int j = 5; j >= i; j--) {
+            System.out.print("*");
+         }
+         for (int j = 1; j < 2 * i - 1; j++) {
+            System.out.print(" ");
+         }
+         for (int j = 5; j >= i; j--) {
+            System.out.print("*");
+         }
+         System.out.println();
+      }
+
+      for (int i = 0; i < 5; i++) {
+         for (int j = 5; j > i; j--) {
+            System.out.print("*");
+         }
+         for (int j = 1; j <= 2 * i; j++) {
+            System.out.print(" ");
+         }
+         for (int j = 5; j > i; j--) {
+            System.out.print("*");
+         }
+         System.out.println();
       }
    }
 }
