f43e789451023b25745f940bd311319616397015
Divyansh Sharma <shan17div@gmail.com>
2026-01-16 22:48:47 +0530

    Day-7

--- JavaBasics/Patterns.java
+++ JavaBasics/Patterns.java
@@ -113,43 +113,57 @@ public class Patterns {
       // a--;
       // }
 
-      for (int i = 0; i < 5; i++) {
-         for (int j = 5; j > i; j--) {
-            System.out.print("*");
-         }
-         for (int j = 1; j <= 2 * i; j++) {
-            System.out.print(" ");
-         }
-         for (int j = 5; j > i; j--) {
-            System.out.print("*");
-         }
-         System.out.println();
-      }
+      // for (int i = 0; i < 5; i++) {
+      // for (int j = 5; j > i; j--) {
+      // System.out.print("*");
+      // }
+      // for (int j = 1; j <= 2 * i; j++) {
+      // System.out.print(" ");
+      // }
+      // for (int j = 5; j > i; j--) {
+      // System.out.print("*");
+      // }
+      // System.out.println();
+      // }
 
-      for (int i = 5; i > 0; i--) {
-         for (int j = 5; j >= i; j--) {
-            System.out.print("*");
-         }
-         for (int j = 1; j < 2 * i - 1; j++) {
-            System.out.print(" ");
-         }
-         for (int j = 5; j >= i; j--) {
-            System.out.print("*");
-         }
-         System.out.println();
-      }
+      // for (int i = 5; i > 0; i--) {
+      // for (int j = 5; j >= i; j--) {
+      // System.out.print("*");
+      // }
+      // for (int j = 1; j < 2 * i - 1; j++) {
+      // System.out.print(" ");
+      // }
+      // for (int j = 5; j >= i; j--) {
+      // System.out.print("*");
+      // }
+      // System.out.println();
+      // }
 
-      for (int i = 0; i < 5; i++) {
-         for (int j = 5; j > i; j--) {
-            System.out.print("*");
-         }
-         for (int j = 1; j <= 2 * i; j++) {
-            System.out.print(" ");
-         }
-         for (int j = 5; j > i; j--) {
-            System.out.print("*");
-         }
-         System.out.println();
-      }
+      // for (int i = 0; i < 5; i++) {
+      // for (int j = 5; j > i; j--) {
+      // System.out.print("*");
+      // }
+      // for (int j = 1; j <= 2 * i; j++) {
+      // System.out.print(" ");
+      // }
+      // for (int j = 5; j > i; j--) {
+      // System.out.print("*");
+      // }
+      // System.out.println();
+      // }
+
+      // for (int i = 1; i <= 5; i++) {
+      // for (int j = 1; j <= 5; j++) {
+      // if (i == 1 || j == 1 || i == 5 || j == 5) {
+      // System.out.print("*");
+      // }else{
+      // System.out.print(" ");
+      // }
+
+      // }
+      // System.out.println();
+      // }
+
+   
    }
 }
